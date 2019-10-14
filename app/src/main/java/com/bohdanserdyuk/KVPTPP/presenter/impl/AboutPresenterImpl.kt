4ea918d403111da2de7b58ed92e81dca3b46095c
BohdanSerdyuk <bohdanserdyuk@gmail.com>
2019-10-14 18:53:25 +0300

    about fragment, float in money

---
+++ app/src/main/java/com/bohdanserdyuk/KVPTPP/presenter/impl/AboutPresenterImpl.kt
@@ -0,0 +1,23 @@
+package com.bohdanserdyuk.KVPTPP.presenter.impl
+
+import android.os.Handler
+import com.bohdanserdyuk.KVPTPP.R
+import com.bohdanserdyuk.KVPTPP.contract.BaseContract
+import com.bohdanserdyuk.KVPTPP.model.repository.impl.PreferencesModelImpl
+import com.bohdanserdyuk.KVPTPP.presenter.BasePresenter
+import com.bohdanserdyuk.KVPTPP.view.impl.MainActivity
+import com.bohdanserdyuk.KVPTPP.view.impl.OnceShowingScreen
+import javax.inject.Inject
+
+class SplashPresenterImpl @Inject constructor(splashModel: BaseContract.SplashModel): BasePresenter<BaseContract.SplashView, BaseContract.SplashModel>(splashModel), BaseContract.SplashPresenter {
+
+    override fun onCreate() {
+        super.onCreate()
+        Handler().postDelayed({
+            view.startActivity(if (getModel(PreferencesModelImpl::class.java).isNewUser)
+                OnceShowingScreen::class.java
+            else
+                MainActivity::class.java)
+        }, view.resolve(R.integer.splash_delay).toLong())
+    }
+}
